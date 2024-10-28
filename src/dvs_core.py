c3043ce9c746e398781070813aefa5642870aa9a
Arian-G <arian.goudar@gmail.com>
2024-10-28 11:11:47 -0500

    Add dvs_network and propogate_dv()

--- src/dvs_core.py
+++ src/dvs_core.py
@@ -15,14 +15,46 @@ class router:
     def update_neighbors( self, router ):
         self.neighbors.add( router )
 
+    def propogate_dv( self, conn ):
+        # TODO: add Bellman-Ford algorithm
+        for neighbor in self.neighbors:
+            message = js.dumps({ self.router_id, self.dv_table })
+            conn.sendall( message.encode( ))
+
 class dvs_core:
     def __init__( self ):
-        self.routers = { }
+        self.routers = { }      # router_id -> router( )
+        self.connections = { }  # router_id -> socket connection
 
     def start_server( self, host = "localhost", port = 8080 ):
         server_socket = s.socket( s.AF_INET, s.SOCK_STREAM )
         server_socket.bind( host, port )
-        server_socket.listen( )
+        server_socket.listen( 7 )
+
+        try:
+            while True:
+                conn, addr = server_socket.accept( )
+                th.Thread( target = self.router_handler, args = ( conn, addr )).start( )
+        except Exception:
+            s = "core_start_server: Unexpected error encountered."
+            return False, s
+        finally:
+            server_socket.close( )
+
+    def router_handler( self, conn, addr ):
+        try:
+            while True:
+                data = conn.recv( 1024 )
+                if not data: break
+                dv_data = js.loads( data.decode( ))
+                for router_id, dv in dv_data.items( ):
+                    self.routers[ router_id ].dv_table.update( dv )
+
+                self.routers[ router_id ].propogate_dv( conn )
+        except Exception:
+            pass
+        finally:
+            conn.close( )
 
     def load( self, filename ):
         try:
